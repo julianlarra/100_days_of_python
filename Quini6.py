a9d1b16592510c04919e9572ce349b9d47528ade
julianlarra <larragueta.julian@gmail.com>
2021-08-18 15:43:20 -0300

    quini en proceso

---
+++ Quini6.py
@@ -0,0 +1,15 @@
+print("Generador de numeros para el Quini6")
+print("El programa le hara 6 preguntas y genera un numero")
+
+mascota= print(input("Cual es el nombre de su mascota? "))
+)
+
+lugar= print(input("Cual es su ciudad o destino favorito? "))
+
+nombre= print(input("Cual es su nombre? "))
+
+pelicula= print(input("Cual es su pelicula favorita? "))
+
+cancion= print(input("Escriba el nombre de una canción: "))
+
+fruta= print(input("Cual es su fruta preferida "))

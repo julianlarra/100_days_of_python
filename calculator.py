dbbe1c0147c019e4644d3c1a621259d90a118908
julianlarra <larragueta.julian@gmail.com>
2022-01-24 14:53:56 -0300

    mas funciones

--- calculator.py
+++ calculator.py
@@ -1,39 +1,57 @@
-
 #  add
 def add(n1, n2):
- return n1 + n2
+    return n1 + n2
+
 
 # subtract
 def subtract(n1, n2):
-  return n1 - n2
+    return n1 - n2
+
 
 # multiply
 def multiply(n1, n2):
-  return n1 * n2
+    return n1 * n2
+
 
 # divide
 def divide(n1, n2):
-  return n1 / n2
+    return n1 / n2
+
 
 operations = {
-"+" : add,
-"-" : subtract,
-"*" : multiply,
-"/" : divide,
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
 }
 
-num1= int(input("What's the first number?: "))
 
-for simbol in operations:
-  print(simbol)
+def calculator():
+    num1 = float(input("What's the first number?: "))
 
-operation_simbol= input("Pick an operation from the line above:\n")
+    for simbol in operations:
+        print(simbol)
+    should_continue = True
 
-num2= int(input("What's the second number?: "))
+    while should_continue:
 
-calculation_function= operations[operation_simbol]
+        operation_simbol = input("Pick an operation:\n")
 
-answer= calculation_function(num1, num2)
+        num2 = float(input("What's the next number?: "))
 
-print(f"{num1} {operation_simbol} {num2} = {answer}")
+        calculation_function = operations[operation_simbol]
+
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_simbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating whit {answer}, or type 'n' to estar a new calculation.:") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
+
 
